29f210aafd50c57433e300c9cdda01085dfe67eb
Song-Jingyu <jingyu_song@outlook.com>
2024-11-03 23:47:55 -0500

    works with GUI

--- docker/run.sh
+++ docker/run.sh
@@ -4,14 +4,21 @@ GROUP_ID=$(id -g)
 PASSWD_FILE=$(mktemp) && echo $(getent passwd $USER_ID) > $PASSWD_FILE
 GROUP_FILE=$(mktemp) && echo $(getent group $GROUP_ID) > $GROUP_FILE
 
-docker run -it \
+
+xhost +local:
+docker run -it --rm \
     -e HOME \
     -u $USER_ID:$GROUP_ID \
     -v $PASSWD_FILE:/etc/passwd:ro \
     -v $GROUP_FILE:/etc/group:ro \
-    -v /mnt/ws-frb/users/jingyuso/docker_data/docker/home:$HOME \
+    -v /mnt/ws-frb/users/jingyuso/navigation/jingyu-docker/docker/home:$HOME \
     -v /mnt:/mnt \
-    --name jingyuso_mmdet3d_1 \
+    --name jingyuso_ros_noetic \
     --gpus all \
     --ipc=host \
-    jingyuso/mmdet3d:latest # TODO: change this image name, container name and home directory accordingly
+     -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XDG_RUNTIME_DIR=/run/user/$USER_ID \
+    -v /run/user/$USER_ID:/run/user/$USER_ID \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    jingyuso/ros-noetic:latest # TODO: change this image name, container name and home directory accordingly
